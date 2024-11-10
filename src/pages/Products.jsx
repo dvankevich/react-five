4de84bf84a277fcd41368740504de2df4fe4280d
dvankevich <dvankevich@gmail.com>
2024-11-10 12:55:25 +0200

    useParams

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,3 +1,14 @@
+import { useParams } from "react-router-dom";
+import { ProductList } from "../components/ProductList";
+import { getProducts } from "../fakeApi";
+
 export default function Products() {
-  return <div>Products Page</div>;
+  const products = getProducts();
+  console.log(useParams());
+
+  return (
+    <main>
+      <ProductList products={products} />
+    </main>
+  );
 }
