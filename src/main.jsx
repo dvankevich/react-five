f20bdea7cf4292414643ff448a2e4940e9399bd4
dvankevich <dvankevich@gmail.com>
2025-12-18 21:58:13 +0200

    add future flag to BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "modern-normalize";
-import { App } from "./components/App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import 'modern-normalize';
+import { App } from './components/App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
       <App />
     </BrowserRouter>
   </React.StrictMode>
