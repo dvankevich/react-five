402b08648425ace238f1c0263b8ad734b2ff86f2
dvankevich <dvankevich@gmail.com>
2025-12-19 19:53:48 +0200

    add useNavigate to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function Home() {
+  const navigate = useNavigate();
   return (
     <main>
-      <h1>Welcome</h1>
+      <h1>Вітаємо на головній сторінці</h1>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto,
         laboriosam placeat incidunt rem illum animi nemo quibusdam quia
         voluptatum voluptate.
       </p>
+      <button onClick={() => navigate('/about')}>
+        Перейти на сторінку "Про нас"
+      </button>
+      <button onClick={() => navigate('/products')}>
+        Перейти на сторінку "Продукти"
+      </button>
     </main>
   );
 }
