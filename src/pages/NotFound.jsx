def8df93c965ed5447ab6f3500ec21864df54615
dvankevich <dvankevich@gmail.com>
2025-12-19 19:30:03 +0200

    change NotFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,16 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export default function NotFound() {
-  return <div>NotFound page</div>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/', { replace: true });
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
+  return <h1>Сторінку не знайдено. Повертаємо на головну...</h1>;
 }
