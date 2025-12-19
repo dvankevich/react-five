fb6c837c8f25bda93c395b3327f5c53c9d33b8a3
dvankevich <dvankevich@gmail.com>
2025-12-19 20:00:26 +0200

    add UserProvider context

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import 'modern-normalize';
 import { App } from './components/App';
 import './index.css';
+import { UserProvider } from './context/userContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -13,7 +14,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         v7_relativeSplatPath: true,
       }}
     >
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
